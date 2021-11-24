8ad8940c37155b7c29c7ba0470707c2d93501c83
joasaavedra <joasaavedra46@gmail.com>
2021-11-24 14:46:17 -0300

    agregando contador

---
+++ src/components/ItemCount.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './itemCount.css'
+
+export const ItemCount = ({ stock, initial, onAdd }) => {
+    const [count, setCount] = useState(initial)
+
+    const increment = () => {
+        if(count < stock){
+            setCount(count + 1)
+        }     
+    }
+
+    const decrement = () => {
+        if(count > 1){
+            setCount(count - 1)
+        }    
+    }
+
+    return (
+        <div>
+            <div className='product'>
+                <img className='productImage' src='https://www.roces.com/pub/media/catalog/product/cache/c7f8b3057cd3dca920be537fa7ff27ff/1/0/101277_002.jpg' alt='Roces M12 LO'/>
+                <p>Roces M12 LO UFS SPASSOV domestic punk</p>
+            </div>
+            <div className='counter'>
+                <button className='quantityBtn' onClick={decrement}>-</button>
+                <p>{count}</p>
+                <button className='quantityBtn' onClick={increment}>+</button>
+            </div>
+            <div className='addToCartBtnContainer'>
+                <button className='addToCartBtn' onClick={() => onAdd(count)}>Add to cart</button>
+            </div>       
+        </div>
+    )
+}
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,15 @@
+import { ItemCount } from './ItemCount'
 import './itemListContainer.css'
 
 export const ItemListContainer = ({greeting}) => {
+    const addToCart = (count) => {
+        console.log(`${count} products has been added`)
+    }
 
-    return <div className='itemListContainer'>
-        <h1 style={{ textDecoration: 'underline' }}>{greeting}</h1>
-    </div>
+    return (
+        <div className='itemListContainer'>
+            <h1>{greeting}</h1>
+            <ItemCount stock='10' initial={1} onAdd={addToCart}/>
+        </div>
+    )
 } 
